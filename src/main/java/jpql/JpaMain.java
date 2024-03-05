dc7faf392c5df19437f1d5b73d06e1b2e43c6990
discphy <dinophy@nate.com>
2024-03-06 08:07:40 +0900

    feat(add) : JPQL 페치 조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,9 @@
 package jpql;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class JpaMain {
@@ -14,31 +17,53 @@ public class JpaMain {
 		tx.begin();
 
 		try {
-			Team team = new Team();
-			team.setName("teamA");
-			em.persist(team);
+			Team teamA = new Team();
+			teamA.setName("teamA");
+			em.persist(teamA);
+
+			Team teamB = new Team();
+			teamB.setName("teamB");
+			em.persist(teamB);
 
 			Member member = new Member();
 			member.setUsername("member1");
 			member.setAge(10);
-			member.setTeam(team);
-			member.setType(MemberType.USER);
+			member.setTeam(teamA);
 
 			Member member2 = new Member();
 			member2.setUsername("member2");
 			member2.setAge(10);
-			member2.setTeam(team);
-			member2.setType(MemberType.USER);
+			member2.setTeam(teamA);
+
+			Member member3 = new Member();
+			member3.setUsername("member3");
+			member3.setAge(10);
+			member3.setTeam(teamB);
 
 			em.persist(member);
 			em.persist(member2);
+			em.persist(member3);
 
-			String query = "select function('group_concat', m.username) from Member m";
-			List<String> result = em.createQuery(query, String.class)
+			em.flush();
+			em.clear();
+
+			String query = "select distinct t from Team t join fetch t.members"; // 지연로딩보다 먼저 작동
+			List<Team> result = em.createQuery(query, Team.class)
 					.getResultList();
 
-			for (String item : result) {
-				System.out.println("item = " + item);
+			for (Team item : result) {
+				System.out.println("member = " + item.getName() + " / " + item.getMembers().size());
+
+				for (Member itemMember : item.getMembers()) {
+					System.out.println("itemMember = " + itemMember);
+
+				}
+
+				// 회원 1, 팀A(SQL)
+				// 회원 2, 팀A(1차캐시)
+				// 회원 3, 팀B(SQL)
+
+				// 회원 100, N + 1
 			}
 
 			tx.commit();
